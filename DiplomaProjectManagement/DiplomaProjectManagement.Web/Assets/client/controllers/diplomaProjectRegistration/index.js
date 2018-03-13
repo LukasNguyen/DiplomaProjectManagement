f3041679ed4174a60acff80095a54b89383a7697
Lukas Nguyen <Dat.NguyenThaiThanh@studentpartner.com>
2018-03-14 01:07:25 +0700

    - Implemented load list student to assign grades by registration times.

--- DiplomaProjectManagement/DiplomaProjectManagement.Web/Assets/client/controllers/diplomaProjectRegistration/index.js
+++ DiplomaProjectManagement/DiplomaProjectManagement.Web/Assets/client/controllers/diplomaProjectRegistration/index.js
@@ -20,7 +20,7 @@
                     return;
                 }
 
-                $.each(response.data, function(i, item) {
+                $.each(response.data, function (i, item) {
                     $('#selectorRegistrationTime')
                         .append($("<option></option>")
                             .attr("value", item.ID)
@@ -28,8 +28,65 @@
                 });
 
                 var selectedValue = $("#selectorRegistrationTime option:selected").val();
-                alert(selectedValue);
 
+                var template = $('#table-template').html();
+                var render = "";
+
+                $.ajax({
+                    type: 'GET',
+                    url: '/DiplomaProjectRegistration/GetStudentToAssignGrades',
+                    data: {
+                        registrationTimeId: selectedValue
+                    },
+                    dataType: 'json',
+                    success: function (response) {
+                        common.startLoading();
+
+                        if (response.data.length === 0) {
+                            $('#tbl-content').html(
+                                '<td colspan="5" style="height:30px; text-align:center"><strong>Không có dữ liệu.</strong></td>');
+                            common.stopLoading();
+                            return;
+                        }
+
+                        $.each(response.data, function (i, item) {
+                            render += Mustache.render(template,
+                                {
+                                    StudentId: item.StudentId,
+                                    StudentName: item.StudentName,
+                                    IntroducedGrades: item.IntroducedGrades,
+                                    ReviewedGrades: item.ReviewedGrades,
+                                    DiplomaProjectName: item.DiplomaProjectName
+                                });
+
+
+                            if (render != '') {
+                                $('#tbl-content').html(render);
+
+                                if (item.RegistrationStatus === 1) {
+                                    $('#btnAssignGrades').show();
+                                } else {
+
+                                    $('#btnAssignGrades').remove();
+
+                                    if (item.RegistrationStatus === 2) {
+                                        $('.introduced-grades').html(item.IntroducedGrades);
+                                        $('.reviewed-grades').html(item.ReviewedGrades);
+                                    } else {
+                                        $('.introduced-grades').html('Chưa thể cập nhật');
+                                        $('.reviewed-grades').html('Chưa thể cập nhật');
+                                    }
+                                }
+
+                                common.stopLoading();
+                            }
+                        });
+                    },
+                    error: function (status) {
+                        console.log(status);
+                        toastr.error('Không có dữ liệu.');
+                    }
+                });
             },
             error: function (status) {
                 console.log(status);
--- DiplomaProjectManagement/DiplomaProjectManagement.Web/Controllers/DiplomaProjectRegistrationController.cs
+++ DiplomaProjectManagement/DiplomaProjectManagement.Web/Controllers/DiplomaProjectRegistrationController.cs
@@ -1,17 +1,24 @@
-﻿using DiplomaProjectManagement.Model.Models;
+﻿using DiplomaProjectManagement.Common;
+using DiplomaProjectManagement.Model.Models;
 using DiplomaProjectManagement.Service;
 using System.Linq;
 using System.Web.Mvc;
+using DiplomaProjectManagement.Common.CustomViewModel;
 
 namespace DiplomaProjectManagement.Web.Controllers
 {
+    [Authorize(Roles = RoleConstants.Lecturer)]
     public class DiplomaProjectRegistrationController : Controller
     {
         private readonly IRegistrationTimeService _registrationTimeService;
+        private readonly IStudentService _studentService;
 
-        public DiplomaProjectRegistrationController(IRegistrationTimeService registrationService)
+        public DiplomaProjectRegistrationController(
+            IRegistrationTimeService registrationService,
+            IStudentService studentService)
         {
             _registrationTimeService = registrationService;
+            _studentService = studentService;
         }
 
         public ActionResult Index()
@@ -30,6 +37,21 @@ namespace DiplomaProjectManagement.Web.Controllers
             else
             {
                 return Json(new { data = Enumerable.Empty<RegistrationTime>() }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public JsonResult GetStudentToAssignGrades(int registrationTimeId)
+        {
+            var currentLecturerId = (int)Session["lecturerId"];
+            var students = _studentService.GetStudentsToAssignGrades(currentLecturerId, registrationTimeId);
+
+            if (students.Any())
+            {
+                return Json(new { data = students }, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(new { data = Enumerable.Empty<LecturerAssignGradesViewModel>() }, JsonRequestBehavior.AllowGet);
             }
         }
     }
