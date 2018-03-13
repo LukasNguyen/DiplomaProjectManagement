802052bdbafd8ba7a8e7d85a771ab1888362cbca
Lukas Nguyen <Dat.NguyenThaiThanh@studentpartner.com>
2018-03-13 21:56:39 +0700

    - Implemented load list registration times to view.

---
+++ DiplomaProjectManagement/DiplomaProjectManagement.Web/Assets/client/controllers/diplomaProjectRegistration/index.js
@@ -0,0 +1,40 @@
+﻿var DiplomaProjectRegistrationController = function () {
+    this.initialize = function () {
+        loadData();
+        registerEvents();
+    }
+
+    function registerEvents() {
+
+    }
+
+    function loadData() {
+        $.ajax({
+            type: 'GET',
+            url: '/DiplomaProjectRegistration/GetRegistrationTimes',
+            dataType: 'json',
+            success: function (response) {
+
+                if (response.length === 0) {
+                    toastr.error('Không có dữ liệu.');
+                    return;
+                }
+
+                $.each(response.data, function(i, item) {
+                    $('#selectorRegistrationTime')
+                        .append($("<option></option>")
+                            .attr("value", item.ID)
+                            .text(item.Name));
+                });
+
+                var selectedValue = $("#selectorRegistrationTime option:selected").val();
+                alert(selectedValue);
+
+            },
+            error: function (status) {
+                console.log(status);
+                toastr.error('Không có dữ liệu.');
+            }
+        });
+    }
+}
---
+++ DiplomaProjectManagement/DiplomaProjectManagement.Web/Controllers/DiplomaProjectRegistrationController.cs
@@ -0,0 +1,36 @@
+﻿using DiplomaProjectManagement.Model.Models;
+using DiplomaProjectManagement.Service;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace DiplomaProjectManagement.Web.Controllers
+{
+    public class DiplomaProjectRegistrationController : Controller
+    {
+        private readonly IRegistrationTimeService _registrationTimeService;
+
+        public DiplomaProjectRegistrationController(IRegistrationTimeService registrationService)
+        {
+            _registrationTimeService = registrationService;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetRegistrationTimes()
+        {
+            var registrationTimes = _registrationTimeService.GetAllRegistrationTimes();
+
+            if (registrationTimes.Any())
+            {
+                return Json(new { data = registrationTimes }, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(new { data = Enumerable.Empty<RegistrationTime>() }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
