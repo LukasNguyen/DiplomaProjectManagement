4c3964c32967483a7094d88f7ef19cedbcde92d9
Lukas Nguyen <Dat.NguyenThaiThanh@studentpartner.com>
2018-03-14 21:08:00 +0700

    - Implemented load list student by registration time.

--- DiplomaProjectManagement/DiplomaProjectManagement.Web/Assets/client/controllers/diplomaProjectRegistration/index.js
+++ DiplomaProjectManagement/DiplomaProjectManagement.Web/Assets/client/controllers/diplomaProjectRegistration/index.js
@@ -28,63 +28,72 @@
                 });
 
                 var selectedValue = $("#selectorRegistrationTime option:selected").val();
+                getStudentToAssignGrades(selectedValue);
 
-                var template = $('#table-template').html();
-                var render = "";
+                $('#selectorRegistrationTime').on('change', function () {
+                    getStudentToAssignGrades(this.value);
+                })
 
-                $.ajax({
-                    type: 'GET',
-                    url: '/DiplomaProjectRegistration/GetStudentToAssignGrades',
-                    data: {
-                        registrationTimeId: selectedValue
-                    },
-                    dataType: 'json',
-                    success: function (response) {
-                        common.startLoading();
+            },
+            error: function (status) {
+                console.log(status);
+                toastr.error('Không có dữ liệu.');
+            }
+        });
+    }
 
-                        if (response.data.length === 0) {
-                            $('#tbl-content').html(
-                                '<td colspan="5" style="height:30px; text-align:center"><strong>Không có dữ liệu.</strong></td>');
-                            common.stopLoading();
-                            return;
+    function getStudentToAssignGrades(selectedValue) {
+
+        var template = $('#table-template').html();
+        var render = "";
+
+        $.ajax({
+            type: 'GET',
+            url: '/DiplomaProjectRegistration/GetStudentToAssignGrades',
+            data: {
+                registrationTimeId: selectedValue
+            },
+            dataType: 'json',
+            success: function (response) {
+                common.startLoading();
+
+                if (response.data.length === 0) {
+                    $('#tbl-content').html(
+                        '<td colspan="5" style="height:30px; text-align:center"><strong>Không có dữ liệu.</strong></td>');
+                    common.stopLoading();
+                    return;
+                }
+
+                $.each(response.data, function (i, item) {
+                    render += Mustache.render(template,
+                        {
+                            StudentId: item.StudentId,
+                            StudentName: item.StudentName,
+                            IntroducedGrades: item.IntroducedGrades,
+                            ReviewedGrades: item.ReviewedGrades,
+                            DiplomaProjectName: item.DiplomaProjectName
+                        });
+
+
+                    if (render != '') {
+                        $('#tbl-content').html(render);
+
+                        if (item.RegistrationStatus === 1) {
+                            $('#btnAssignGrades').show();
+                        } else {
+
+                            $('#btnAssignGrades').remove();
+
+                            if (item.RegistrationStatus === 2) {
+                                $('.introduced-grades').html(item.IntroducedGrades);
+                                $('.reviewed-grades').html(item.ReviewedGrades);
+                            } else {
+                                $('.introduced-grades').html('Chưa thể cập nhật');
+                                $('.reviewed-grades').html('Chưa thể cập nhật');
+                            }
                         }
 
-                        $.each(response.data, function (i, item) {
-                            render += Mustache.render(template,
-                                {
-                                    StudentId: item.StudentId,
-                                    StudentName: item.StudentName,
-                                    IntroducedGrades: item.IntroducedGrades,
-                                    ReviewedGrades: item.ReviewedGrades,
-                                    DiplomaProjectName: item.DiplomaProjectName
-                                });
-
-
-                            if (render != '') {
-                                $('#tbl-content').html(render);
-
-                                if (item.RegistrationStatus === 1) {
-                                    $('#btnAssignGrades').show();
-                                } else {
-
-                                    $('#btnAssignGrades').remove();
-
-                                    if (item.RegistrationStatus === 2) {
-                                        $('.introduced-grades').html(item.IntroducedGrades);
-                                        $('.reviewed-grades').html(item.ReviewedGrades);
-                                    } else {
-                                        $('.introduced-grades').html('Chưa thể cập nhật');
-                                        $('.reviewed-grades').html('Chưa thể cập nhật');
-                                    }
-                                }
-
-                                common.stopLoading();
-                            }
-                        });
-                    },
-                    error: function (status) {
-                        console.log(status);
-                        toastr.error('Không có dữ liệu.');
+                        common.stopLoading();
                     }
                 });
             },
