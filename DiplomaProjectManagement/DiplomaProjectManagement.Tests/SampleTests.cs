836dedfa0e45b2ddfa42acc3a90d1f0489d5ed49
Lukas Nguyen <Dat.NguyenThaiThanh@studentpartner.com>
2018-06-13 00:38:14 +0700

    Added test project to test CI/CD server.

---
+++ DiplomaProjectManagement/DiplomaProjectManagement.Tests/SampleTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiplomaProjectManagement.Tests
+{
+    [TestFixture]
+    public class SampleTests
+    {
+        [Test]
+        public void Should_always_return_true()
+        {
+            Assert.AreEqual(true, true);
+        }
+    }
+}
